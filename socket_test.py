34fb3bc8ee7e26053d3d555cc9166dc08f7b2c6b
Kit <kittsville@googlemail.com>
2025-07-27 21:46:33 +0100

    Add socket test script

---
+++ socket_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import socket_setup
+import logging
+import time
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+socket_setup.setup()
+
+logging.info("Sending code 1011 all sockets on")
+GPIO.output (13, True)
+GPIO.output (16, False)
+GPIO.output (15, True)
+GPIO.output (11, True)
+
+time.sleep(2)
+
+logging.info("Sending code 0011 all sockets off")
+GPIO.output (13, False)
+GPIO.output (16, False)
+GPIO.output (15, True)
+GPIO.output (11, True)
